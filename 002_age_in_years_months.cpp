4f00a356608a9caae302c6531d1bf8fc632af832
Chirayu1411Mitra <mitrachirayu@gmail.com>
2024-08-22 19:52:33 +0530

    age in days

--- 002_age_in_years_months.cpp
+++ 002_age_in_years_months.cpp
@@ -15,8 +15,13 @@ void calculate_age(int birth_month,int birth_year,int Current_month,int Current_
      age_years= (Current_year-birth_year)-1;
     age_months=(12-birth_month)+Current_month;
 }
-void print_age(int age_months,int age_years){
-    cout<<"Your age is "<<age_years<<" years and "<<age_months<<" months"; 
+void days(int &days1, int age_years){
+    days1= age_years*365;
+
+    
+}
+void print_age(int age_months, int days1, int age_years){
+    cout<<"Your age is"<< days1<< "ans years is "<< age_years; 
 }
 int main(){
     int birth_month;
@@ -25,9 +30,11 @@ int main(){
     int Current_year;
     int age_months;
     int age_years;
+    int days1;
 
   inputs(birth_month, birth_year, Current_month, Current_year, age_months, age_years);
   calculate_age(birth_month, birth_year, Current_month, Current_year, age_months, age_years);
-  print_age(age_months, age_years);
+  days(age_years, days1);
+  print_age(age_months, days1, age_years);
   return 0;
 }
